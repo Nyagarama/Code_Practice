adda9b9bbc0e267355523fba68e3c4abc5514c59
Nyagarama <mochristopher27@gmail.com>
2024-02-04 07:40:35 -0800

    indexing

---
+++ Practice_Pointer_Array/index.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x[100]; /* declare a 100-interger array */
+	int t;
+
+	x[t] = t;
+
+	for (t=0; t<100; t++)
+	printf("%d \n", t);
+
+	return (0);
+}
